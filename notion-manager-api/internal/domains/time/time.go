edf4134ac3fb48c9980ad661bfe16c0e1cba464b
Mark <mark.corray.off@gmail.com>
2025-04-30 20:00:58 +0300

    hotfix: configs

--- notion-manager-api/internal/domains/time/time.go
+++ notion-manager-api/internal/domains/time/time.go
@@ -1,8 +1,12 @@
 package feedback
 
 import (
+	"context"
+	"fmt"
+
 	notion_repo "github.com/Corray333/employee_dashboard/internal/domains/time/repositories/notion"
 	postgres_repo "github.com/Corray333/employee_dashboard/internal/domains/time/repositories/postgres"
+	"github.com/Corray333/employee_dashboard/internal/domains/time/repositories/sheets"
 	"github.com/Corray333/employee_dashboard/internal/domains/time/service"
 	"github.com/Corray333/employee_dashboard/internal/domains/time/transport"
 	"github.com/Corray333/employee_dashboard/internal/postgres"
@@ -22,12 +26,12 @@ func NewTimeController(grpcServer *grpc.Server, store *postgres.PostgresClient, 
 
 	postgresRepo := postgres_repo.NewTimePostgresRepository(store)
 	notionRepo := notion_repo.NewTimeNotionRepository(notionClient)
-	// sheetsRepo := sheets.NewTimeSheetsRepository(sheetsClient)
-	// times, err := postgresRepo.ListTimes(context.Background(), 0, 9000)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(sheetsRepo.UpdateSheetsTimes(context.Background(), times))
+	sheetsRepo := sheets.NewTimeSheetsRepository(sheetsClient)
+	times, err := postgresRepo.ListTimes(context.Background(), 0, 9000)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(sheetsRepo.UpdateSheetsTimes(context.Background(), times))
 
 	service := service.NewTaskService(service.WithPostgresRepository(postgresRepo), service.WithNotionRepository(notionRepo))
 
