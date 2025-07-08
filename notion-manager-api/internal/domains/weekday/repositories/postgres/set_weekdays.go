3d70e3c6a7b5ead3545826f037cd95e6ffff8e07
Mark <mark.corray.off@gmail.com>
2025-07-08 14:41:22 +0300

    FUCKING HOTFIX

--- notion-manager-api/internal/domains/weekday/repositories/postgres/set_weekdays.go
+++ notion-manager-api/internal/domains/weekday/repositories/postgres/set_weekdays.go
@@ -17,17 +17,31 @@ func (r *WeekdayPostgresRepository) SetWeekday(ctx context.Context, weekday *wee
 	}
 
 	_, err = r.DB().NamedExecContext(ctx, `
-		INSERT INTO weekdays (weekday_id, employee_id, category, start_time, end_time, reason, created_at, updated_at, notified)
-		VALUES (:weekday_id, :employee_id, :category, :start_time, :end_time, :reason, :created_at, :updated_at, :notified)
-		ON CONFLICT (weekday_id) DO UPDATE SET
+		INSERT INTO weekdays (
+			weekday_id, employee_id, category,
+			start_time, end_time, reason,
+			created_at, updated_at, notified
+		)
+		VALUES (
+			:weekday_id, :employee_id, :category,
+			:start_time, :end_time, :reason,
+			:created_at, :updated_at, :notified
+		)
+		ON CONFLICT (weekday_id) DO UPDATE
+		SET
 			employee_id = EXCLUDED.employee_id,
-			category = EXCLUDED.category,
-			start_time = EXCLUDED.start_time,
-			end_time = EXCLUDED.end_time,
-			reason = EXCLUDED.reason,
-			created_at = EXCLUDED.created_at,
-			updated_at = EXCLUDED.updated_at,
-			notified = EXCLUDED.notified
+			category    = EXCLUDED.category,
+			start_time  = EXCLUDED.start_time,
+			end_time    = EXCLUDED.end_time,
+			reason      = EXCLUDED.reason,
+			created_at  = EXCLUDED.created_at,
+			updated_at  = EXCLUDED.updated_at,
+			notified    = CASE
+							-- IS DISTINCT FROM корректно работает с NULL:
+							WHEN EXCLUDED.updated_at IS DISTINCT FROM weekdays.updated_at
+							THEN EXCLUDED.notified     -- время изменилось → обновляем флаг
+							ELSE weekdays.notified     -- время то же → оставляем как было
+						END;
 	`, weekdayDBFromEntity(weekday))
 	if err != nil {
 		slog.Error("Error setting weekday", "error", err)
