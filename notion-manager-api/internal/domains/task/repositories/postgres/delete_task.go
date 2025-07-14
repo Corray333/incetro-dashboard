e7e0fff7410d87e9d666e5a20ebd4321da33e072
Mark <mark.corray.off@gmail.com>
2025-07-14 22:50:02 +0300

    fix: logs

--- notion-manager-api/internal/domains/task/repositories/postgres/delete_task.go
+++ notion-manager-api/internal/domains/task/repositories/postgres/delete_task.go
@@ -9,8 +9,7 @@ import (
 
 // DeleteTask deletes a task from the database by its ID
 func (r *TaskPostgresRepository) DeleteTask(ctx context.Context, taskID uuid.UUID) error {
-	slog.Info("Deleting task", "taskID", taskID)
-	query := `DELETE FROM tasks WHERE id = $1`
+	query := `DELETE FROM tasks WHERE task_id = $1`
 	_, err := r.DB().ExecContext(ctx, query, taskID)
 	if err != nil {
 		slog.Error("Error deleting task", "error", err)
--- notion-manager-api/internal/domains/time/repositories/postgres/delete_time.go
+++ notion-manager-api/internal/domains/time/repositories/postgres/delete_time.go
@@ -2,13 +2,18 @@ package postgres
 
 import (
 	"context"
+	"log/slog"
 
 	"github.com/google/uuid"
 )
 
 // DeleteTime deletes a time entry from the database by its ID
 func (r *TimePostgresRepository) DeleteTime(ctx context.Context, timeID uuid.UUID) error {
-	query := `DELETE FROM times WHERE id = $1`
+	query := `DELETE FROM times WHERE time_id = $1`
 	_, err := r.DB().ExecContext(ctx, query, timeID)
-	return err
+	if err != nil {
+		slog.Error("Error deleting time", "error", err)
+		return err
+	}
+	return nil
 }
