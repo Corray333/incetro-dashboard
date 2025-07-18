8df9a8f5d7705053e46dc09bbf3e1f3a021b8b95
Mark <mark.corray.off@gmail.com>
2025-07-18 17:34:04 +0300

    fix: notion listing incorrect times format

--- tg-task-parser/internal/repositories/storage/get_employee_tg_id.go
+++ tg-task-parser/internal/repositories/storage/get_employee_tg_id.go
@@ -7,10 +7,10 @@ import (
 	"github.com/google/uuid"
 )
 
-func (r *PostgresRepository) GetEmployeeTgIDByID(ctx context.Context, employeeID uuid.UUID) (int64, error) {
+func (r *PostgresRepository) GetEmployeeTgIDByID(ctx context.Context, profileID uuid.UUID) (int64, error) {
 	var tgID int64
-	if err := r.db.Get(&tgID, "SELECT tg_id FROM employees WHERE employee_id = $1", employeeID); err != nil {
-		slog.Error("Error while getting employee tg_id by ID", "employee_id", employeeID, "error", err)
+	if err := r.db.Get(&tgID, "SELECT tg_id FROM employees WHERE profile_id = $1", profileID); err != nil {
+		slog.Error("Error while getting employee tg_id by ID", "profile_id", profileID, "error", err)
 		return 0, err
 	}
 	return tgID, nil
