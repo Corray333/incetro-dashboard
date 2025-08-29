6eeed2697d8ef0266e332f3854b6f1f0c25c9a7f
Mark <mark.corray.off@gmail.com>
2025-08-29 16:29:32 +0300

    fix: user getter

--- tg-task-parser/internal/repositories/base_service/get_employee.go
+++ tg-task-parser/internal/repositories/base_service/get_employee.go
@@ -1,11 +1,15 @@
 package base_service
 
-import "context"
+import (
+	"context"
+	"log/slog"
+)
 
 func (r *BaseService) GetEmployeeByTgUsername(ctx context.Context, username string) (string, error) {
 	employeeID := ""
 	err := r.db.QueryRow("SELECT employee_id FROM employees WHERE tg_username = $1", username).Scan(&employeeID)
 	if err != nil {
+		slog.Error("Notion error while getting employee ID", "error", err, "username", username)
 		return "", err
 	}
 	return employeeID, nil
