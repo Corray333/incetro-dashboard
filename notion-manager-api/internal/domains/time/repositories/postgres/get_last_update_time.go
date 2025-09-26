4ca09b847585e2cf5ae946d3f53505c89121be62
Mark Anikin <mark.corray.off@gmail.com>
2025-09-26 09:22:17 +0300

    fix: logs

--- notion-manager-api/internal/domains/time/repositories/postgres/get_last_update_time.go
+++ notion-manager-api/internal/domains/time/repositories/postgres/get_last_update_time.go
@@ -8,7 +8,11 @@ import (
 
 func (s *TimePostgresRepository) GetTimeLastUpdateTime(ctx context.Context) (time.Time, error) {
 	lastUpdateTime := time.Time{}
-	if err := s.DB().Get(&lastUpdateTime, "SELECT COALESCE(MAX(last_update), '2000-01-01 00:00:00+00') FROM times LIMIT 1"); err != nil {
+	// if err := s.DB().Get(&lastUpdateTime, "SELECT COALESCE(MAX(last_update), '2000-01-01 00:00:00+00') FROM times LIMIT 1"); err != nil {
+	// 	slog.Error("Error getting time last update time", "error", err)
+	// 	return lastUpdateTime, err
+	// }
+	if err := s.DB().Get(&lastUpdateTime, "SELECT COALESCE(times_db_last_sync, '2000-01-01 00:00:00+00') FROM system LIMIT 1"); err != nil {
 		slog.Error("Error getting time last update time", "error", err)
 		return lastUpdateTime, err
 	}
@@ -17,10 +21,10 @@ func (s *TimePostgresRepository) GetTimeLastUpdateTime(ctx context.Context) (tim
 }
 
 func (s *TimePostgresRepository) SetTimeLastUpdateTime(ctx context.Context, lastUpdateTime time.Time) error {
-	// _, err := s.DB().Exec("UPDATE system SET times_db_last_sync = $1", lastUpdateTime)
-	// if err != nil {
-	// 	slog.Error("Error setting time last update time", "error", err)
-	// 	return err
-	// }
+	_, err := s.DB().Exec("UPDATE system SET times_db_last_sync = $1", lastUpdateTime)
+	if err != nil {
+		slog.Error("Error setting time last update time", "error", err)
+		return err
+	}
 	return nil
 }
